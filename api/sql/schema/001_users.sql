7ae887063e21fc7f4db0459556ad9e70b965e93e
Tikkaaa3 <tikkaaa3@gmail.com>
2026-02-04 19:44:09 +0100

    feat(api): initialize backend structure with SQLC

---
+++ api/sql/schema/001_users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+);
